0f1663b97730c3b1bd1a8f0bfdc50c22d165c8cc
Samuel Peppard <samuel_peppard@comcast.com>
2021-01-23 22:13:21 -0500

    first commit

---
+++ src/build/precache-manifest.fd0ce8be2dd809d1df932c2192afb21d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7a1d432a2b88f839e5d6",
+    "url": "/static/js/main.25562a57.chunk.js"
+  },
+  {
+    "revision": "52008e4d04e5d6354b16",
+    "url": "/static/js/2.be8fd421.chunk.js"
+  },
+  {
+    "revision": "7a1d432a2b88f839e5d6",
+    "url": "/static/css/main.782a66cb.chunk.css"
+  },
+  {
+    "revision": "b8998feb8f04f3828bb1d2360c575d76",
+    "url": "/index.html"
+  }
+];
